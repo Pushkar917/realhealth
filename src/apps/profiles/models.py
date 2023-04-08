4bba37c372e79fa488d4ac8df73577de8e29ae08
Pushkar917 <pshkranand40@gmail.com>
2023-04-08 15:27:20 +0530

    migrations added

--- src/apps/profiles/models.py
+++ src/apps/profiles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from apps.common.models import TimeStampedUUIDModel
+from apps.common.models import TimestampedUUIDModel
 from django_countries.fields import CountryField
 from django.core.validators import RegexValidator
 
@@ -11,18 +11,18 @@ User = get_user_model()
 # Create your models here.
 
 class Gender(models.TextChoices):
-    MALE = "Male", _("Male")
-    FEMALE = "Female", _("Female")
-    OTHER = "Other", _('Other')
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
 
 
-class Profile(TimeStampedUUIDModel):
+class Profile(TimestampedUUIDModel):
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,11}$', message="Phone number must be entered in the format: '+999999999'. Up to 11 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
     about_me = models.TextField(verbose_name="About Me", default="say something about yourself")
     gender = models.CharField(verbose_name="Gender", choices=Gender.choices, default=Gender.OTHER)
-    country = CountryField(verbose_name=_"Country", default="IN", blank=False, null=False)
+    country = CountryField(verbose_name="Country", default="IN", blank=False, null=False)
 
 
     def __str__(self):
